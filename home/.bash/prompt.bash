7736bbbbc8a037f91c4d075186cfc51363a2fc6d
Dani Rodríguez <dani@danirod.es>
2018-01-27 17:47:24 +0100

    [bash] remove try_source
    I forgot that ~/.bash_profile doesn't get read on Linux when a terminal
    gets opened because that' just a macOS thing to simulate a login shell
    on an OS that does not respect login shells anyway.

--- home/.bash/prompt.bash
+++ home/.bash/prompt.bash
@@ -6,13 +6,13 @@ __prompt() {
   local red='\[\e[1;31m\]'
   local reset='\[\e[0m\]'
 
-  PS1="\n$blue\u$reset@$cyan\h$reset:$green\w"
+  PS1="$blue\u$reset@$cyan\h$reset:$green\w"
 
   if [ $EXIT_CODE != 0 ]; then
     PS1="$PS1 $red[$EXIT_CODE]"
   fi
 
-  PS1="$PS1$reset\n-> "
+  PS1="$PS1$reset -> "
 }
 
 export PROMPT_COMMAND=__prompt
