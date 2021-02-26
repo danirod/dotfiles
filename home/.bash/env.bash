af5933c4ac1ec6fedf1ed020fa1447fe09559624
Dani Rodríguez <dani@danirod.es>
2021-02-26 17:32:31 +0100

    sync dot files in work laptop

--- home/.bash/env.bash
+++ home/.bash/env.bash
@@ -15,6 +15,23 @@ elif [ -x $(command -v dillo) ]; then
     export BROWSER=dillo  # dillo
 fi
 
+# Git: diff-highlighter
+if [ -x /usr/local/share/git-core/contrib/diff-highlight/diff-highlight ]; then
+	export PATH=$PATH:/usr/local/share/git-core/contrib/diff-highlight
+elif [ -x /usr/share/git-core/contrib/diff-highlight/diff-highlight ]; then
+	export PATH=$PATH:/usr/share/git-core/contrib/diff-highlight
+fi
+
+# rbenv
+if [ -x $(command -v rbenv) ]; then
+	eval "$(rbenv init -)"
+fi
+
+# nodenv
+if [ -x $(command -v nodenv) ]; then
+	eval "$(nodenv init -)"
+fi
+
 export RIPGREP_CONFIG_PATH=~/.config/ripgrep/config
 
 export HTTPIE_CONFIG_DIR=~/.config/httpie/
