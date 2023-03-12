45359e76ef95a9548e506f161598000843c222b3
Dani Rodríguez <dani@danirod.es>
2023-03-12 17:26:36 +0100

    i3: toolmenu

---
+++ i3/.config/rofi/tools.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+COUNT=3
+MARGIN="margin"
+SCROT="scrot"
+SCROT_REGION="scrot-region"
+options="$MARGIN\n$SCROT\n$SCROT_REGION"
+chosen=$(echo -en "$options" | rofi -dmenu -i -theme ~/.config/rofi/confirm.rasi -theme-str "listview { lines: $COUNT; }" -hover-select -me-select-entry '' -me-accept-entry MousePrimary)
+case "$chosen" in
+    $MARGIN)
+        i3-msg "gaps right current toggle 400"
+        ;;
+    $SCROT)
+        scrot "$HOME/images/screenshots/%Y%m%d-%H%M%S.png" -e 'feh $f'
+        ;;
+    $SCROT_REGION)
+        scrot -s "$HOME/images/screenshots/%Y%m%d-%H%M%S.png" -e 'feh $f'
+        ;;
+esac
