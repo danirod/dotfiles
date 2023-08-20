d16e11602ae04a1819ac5396e776cc454011c803
Dani Rodríguez <dani@danirod.es>
2023-08-20 13:29:45 +0200

    [i3] retro colorscheme

--- i3/.config/rofi/confirm.sh
+++ i3/.config/rofi/confirm.sh
@@ -2,7 +2,7 @@
 
 YES="yes ฅ(^◕ᴥ◕^)ฅ"
 NO="no (´-ω-\`)"
-chosen=$(echo -en "$YES\n$NO" | rofi -dmenu -i -theme ~/.config/rofi/confirm.rasi -theme-str "listview { lines: 2; }" -hover-select -me-select-entry '' -me-accept-entry MousePrimary)
+chosen=$(echo -en "$YES\n$NO" | rofi -dmenu -i -theme ~/.config/rofi/confirm.rasi -theme-str "listview { lines: 2; }" -hover-select -me-select-entry '' -me-accept-entry MousePrimary -selected-row 1)
 if [ "$chosen" == "$YES" ]; then
     exit 0
 else
